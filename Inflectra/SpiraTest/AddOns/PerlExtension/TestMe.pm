e3669d78306008e0606a9fa3778b12946d875694
asandman2 <asandman2@5d4b435c-4d46-d949-aab2-e601ca53af14>
2009-10-27 15:35:08 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@9227 5d4b435c-4d46-d949-aab2-e601ca53af14

---
+++ Inflectra/SpiraTest/AddOns/PerlExtension/TestMe.pm
@@ -0,0 +1,28 @@
+package Inflectra::SpiraTest::Addons::PerlExtension::TestMe;
+
+use strict;
+use warnings;
+our $VERSION = '1.00';
+use base 'Exporter';
+our @EXPORT = qw( add subtract multiply );
+
+#The addition function
+sub add
+{
+  my ($num1,  $num2) = @_;
+  return $num1 + $num2;
+}
+
+#The subtraction function
+sub subtract
+{
+  my ($num1,  $num2) = @_;
+  return $num1 - $num2;
+}
+
+#The multiplication function
+sub multiply
+{
+  my ($num1,  $num2) = @_;
+  return $num1 * $num2;
+}
